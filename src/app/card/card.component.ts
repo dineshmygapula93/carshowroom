4ab5b10d23ee937a510680056a872a729acae17b
divyasri mygapula@LAPTOP-9JOAST2M <dineshmygapula.mca@gmail.com>
2025-05-15 14:59:31 +0530

    component v1

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: false,
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+@Input() url:string =''
+@Input() title:string=''
+@Input() cost:string=""
+}
